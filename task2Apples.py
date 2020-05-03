0607a6f693bf144ceb4346716a32c2cd2e177fa3
Nurgazy24 <nuroskonbaev24ray@gmail.com>
2020-05-03 14:47:49 +0600

    Add task2Apples file

---
+++ task2Apples.py
@@ -0,0 +1,4 @@
+Students = int(input("Введите кол-во студентов: "))
+Apples = int(input("Введите кол-во яблок: "))
+print("У каждого студента: ",(Apples // Students))
+print("Осталось в корзине: ", (Apples % Students))
